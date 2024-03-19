fd38b57e2c8541ef4f778cd50b6a7e90501d7d66
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-03-19 16:55:01 +0200

    Tehtävät 3.7-3.8

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(function (tokens, req, res) {
+    let postcontent = ""
+    if (tokens.method(req, res) === "POST"){
+        postcontent = JSON.stringify(req.body)
+    }
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.method(req, res) === "POST" ? JSON.stringify(req.body) : ""
+    ].join(' ')
+}))
 
 let persons = [
     {
