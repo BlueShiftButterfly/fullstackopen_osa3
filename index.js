35decdcb76fb31fb4623ce368afa57e069a522ce
BlueShiftButterfly <34643088+BlueShiftButterfly@users.noreply.github.com>
2024-07-11 15:25:37 +0300

    Tehtävät 3.15, 3.16, 3.17, 3.18

--- index.js
+++ index.js
@@ -25,29 +25,6 @@ app.use(morgan(function (tokens, req, res) {
     ].join(" ")
 }))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
 const dateOptions = {
     weekday: "short",
     year: "numeric",
@@ -68,14 +45,11 @@ const GetCurrentDate = () => {
     )
 }
 
-const GeneratePersonID = () => {
-    return ( Math.floor(Math.random() * 1000000000) )
-}
-
 app.get("/api/persons", (request, response) => {
     Person.find({}).then(result => {
         response.json(result)
     })
+    .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
@@ -87,18 +61,38 @@ app.get("/info", (request, response) => {
             `
         )
     })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    Person.findById(id).then(result => {
+app.get("/api/persons/:id", (request, response, next) => {
+    Person.findById(request.params.id).then(result => {
         if (result) {
             response.json(result)
         } else {
             response.status(404).end()
         }
     })
+    .catch(error => next(error))
 })
+
+app.put("/api/persons/:id", (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true }).then(result => {
+        if (result) {
+            response.json(result)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
 
 app.delete("/api/persons/:id", (request, response) => {
     Person.findByIdAndDelete(request.params.id)
@@ -111,18 +105,6 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
     const body = request.body
 
-    if (!body.name) {
-        return response.status(400).json({ 
-            error: "name missing" 
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({ 
-            error: "number missing" 
-        })
-    }
-
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -131,7 +113,26 @@ app.post("/api/persons", (request, response) => {
     person.save().then(result => {
         response.json(result)
     })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
